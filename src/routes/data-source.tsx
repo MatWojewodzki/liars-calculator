0afe13776825a740f38c2290783145d3b6bec156
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-07-11 18:40:11 +0200

    Clarify that the script was used specifically for poker hand probability calculations

--- src/routes/data-source.tsx
+++ src/routes/data-source.tsx
@@ -9,7 +9,7 @@ function RouteComponent() {
       <div className="max-w-5xl mx-auto px-4 py-16">
         <h1 className="text-2xl font-semibold">Data source</h1>
         <p className="mt-4">
-          All probability data used on this website was calculated using my own script written in Python.
+          All poker hand probability data used on this website has been calculated using my own script written in Python.
           The source code is available on <a
               href="https://github.com/MatWojewodzki/liars-poker-probabilities"
               className="underline"
