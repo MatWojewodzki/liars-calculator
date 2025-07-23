78451c5a816ac92573b934e6bd34db1b14c5da14
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-07-23 21:02:27 +0200

    Increase the spacing in the Footer and decrease the font size of the copyright notice

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,14 +12,18 @@ function Footer() {
             className={classNames(
                 "text-center py-16",
                 "bg-linear-to-t from-neutral-50 to-neutral-100",
-                "flex flex-col items-center justify-center gap-2"
+                "flex flex-col items-center justify-center gap-4"
             )}
         >
-            <p>Liar's Calculator &copy; 2025{currentYear()} Mateusz Wojewódzki</p>
-            <p className="text-sm text-neutral-500">Source code available on <a
-                href="https://github.com/MatWojewodzki/liars-calculator"
-                className="underline"
-            >GitHub</a></p>
+            <p className="text-sm">
+                Liar's Calculator &copy; 2025{currentYear()} Mateusz Wojewódzki
+            </p>
+            <p className="text-sm text-neutral-500">
+                Source code available on <a
+                    href="https://github.com/MatWojewodzki/liars-calculator"
+                    className="underline"
+                >GitHub</a>
+            </p>
         </footer>
     )
 }
