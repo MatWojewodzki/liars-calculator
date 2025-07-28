5c96547f4e0a65d84a8bbb2b2ea6da56d1f0c22e
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-07-28 13:46:56 +0200

    Update data source description and GitHub link

--- src/routes/data-source.tsx
+++ src/routes/data-source.tsx
@@ -9,9 +9,9 @@ function RouteComponent() {
       <div className="max-w-5xl mx-auto px-4 py-16">
         <h1 className="text-2xl font-semibold">Data source</h1>
         <p className="mt-4">
-          All poker hand probability data used on this website has been calculated using my own script written in Python.
+          All poker hand probability data used on this website has been calculated using a Python script.
           The source code is available on <a
-              href="https://github.com/MatWojewodzki/liars-poker-probabilities"
+              href="https://github.com/MatWojewodzki/liars-poker-probabilities/blob/main/probability_data_generator.py"
               className="underline"
           >
             GitHub
